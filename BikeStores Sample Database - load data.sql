fa19e653203fcfadc68d1f2cedb79b0bf01e8cce
Obidee <120103380+Obidee@users.noreply.github.com>
2023-08-10 16:52:32 +0100

    Update BikeStores Sample Database - load data.sql

--- BikeStores Sample Database - load data.sql
+++ BikeStores Sample Database - load data.sql
@@ -1,13 +1,3 @@
-/*
---------------------------------------------------------------------
-© 2017 sqlservertutorial.net All Rights Reserved
---------------------------------------------------------------------
-Name   : BikeStores
-Link   : http://www.sqlservertutorial.net/load-sample-database/
-Version: 1.0
---------------------------------------------------------------------
-*/
-
 use BikeStores;
   
 -- brands table --
